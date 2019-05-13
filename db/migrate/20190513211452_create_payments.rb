59ca6db96315ba19275a0b322cdb59db769830ff
1-8192 <allegranzia@gmail.com>
2019-05-13 18:35:36 -0400

    building out models and controllers

---
+++ db/migrate/20190513211452_create_payments.rb
@@ -0,0 +1,11 @@
+class CreatePayments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :payments do |t|
+      t.float :amount
+      t.belongs_to :user
+      t.belongs_to :dog
+
+      t.timestamps
+    end
+  end
+end

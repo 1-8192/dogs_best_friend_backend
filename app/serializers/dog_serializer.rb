570805f17069eeeb2c3f5a4024128189b4d381e8
1-8192 <allegranzia@gmail.com>
2019-05-15 09:30:01 -0400

    set up dog index route

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -1,3 +1,7 @@
 class DogSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :image_url, :chip_id, :sex, :breed, :age, :is_vaccinated, :at_risk, :shelter_id
+
+  belongs_to :shelter
+  has_many :payments
+  has_many :users, through: :payments
 end

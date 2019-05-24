4185d3327eb2fd78bee900b84c3fcefbad632867
1-8192 <allegranzia@gmail.com>
2019-05-24 10:42:37 -0400

    implemented stripe payment

--- app/controllers/api/v1/charges_controller.rb
+++ app/controllers/api/v1/charges_controller.rb
@@ -3,32 +3,43 @@ require 'securerandom'
 
 class Api::V1::ChargesController < ApplicationController
 
+  def new
+  end
+
   def create
 
   Stripe.api_key = 'sk_test_Sh3JmNAY6uuHzkyf2Gh51gVO00W8XJk3im'
   ip_key = SecureRandom.uuid
 
-  customer = Stripe::Customer.create(
-  :email => current_user.email,
-  :source => params[:token]
-  )
+  begin
 
-  charge = Stripe::Charge.create(
-  :customer => customer.id,
-  :amount => params[:amount],
-  :description => params[:description],
-  :currency => params[:currency],
-  :idempotency_key => ip_key
-  )
+    customer = Stripe::Customer.create(
+    :email => current_user.email,
+    :source => params[:charge][:token]
+    )
+
+    puts customer
+
+    charge = Stripe::Charge.create({
+    :customer => customer.id,
+    :amount => params[:charge][:amount],
+    :description => params[:charge][:description],
+    :currency => params[:charge][:currency],
+    }, {
+    :idempotency_key => ip_key
+    })
+
+    puts charge
 
   rescue Stripe::CardError => e
     render json: { message: 'oops'}, status: :not_acceptable
   end
+end
 
   private
 
-  def charge_params
-    params.require(:charge).permit(:amount, :description, :currency)
-  end
+  # def charge_params
+  #   params.require(:charge).permit(:amount, :currency, :description, :chargeToken)
+  # end
 
 end
